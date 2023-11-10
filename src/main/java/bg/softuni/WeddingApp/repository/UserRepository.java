06e2151880ca4f660ce86434ce18f22f520fd50a
VesyMincheva <v.mincheva2013@gmail.com>
2023-11-10 21:00:09 +0200

    Create registration functionality.

--- src/main/java/bg/softuni/WeddingApp/repository/UserRepository.java
+++ src/main/java/bg/softuni/WeddingApp/repository/UserRepository.java
@@ -4,6 +4,11 @@ import bg.softuni.WeddingApp.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserByUsername(String username);
 }
--- src/main/java/bg/softuni/WeddingApp/web/UserController.java
+++ src/main/java/bg/softuni/WeddingApp/web/UserController.java
@@ -1,14 +1,48 @@
 package bg.softuni.WeddingApp.web;
 
+import bg.softuni.WeddingApp.model.dto.UserRegistrationDTO;
+import bg.softuni.WeddingApp.service.impl.UserServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
 
+    private final UserServiceImpl userService;
+
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @ModelAttribute("userRegistrationDTO")
+    public UserRegistrationDTO initRegistrationDto (){
+        return new UserRegistrationDTO();
+    }
+
     @GetMapping("/register")
     public String register() {
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid UserRegistrationDTO userRegistrationDTO,
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors() || !this.userService.register(userRegistrationDTO)){
+            redirectAttributes.addFlashAttribute("userRegistrationDTO", userRegistrationDTO);
+            redirectAttributes.addFlashAttribute(
+                    "org.springframework.validation.BindingResult.userRegistrationDTO", bindingResult);
+
+            return "redirect:/register";
+        }
+
+        return "redirect:/login";
     }
 
     @GetMapping("/login")
